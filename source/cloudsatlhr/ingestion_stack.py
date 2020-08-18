7f675c0503f40862befd103c0bbefcff41ac8c5a
Jake Hendy <me@jakehendy.com>
2020-08-18 13:01:20 +0100

    Attach lambda directly to 5min event

--- source/cloudsatlhr/ingestion_stack.py
+++ source/cloudsatlhr/ingestion_stack.py
@@ -7,14 +7,6 @@ class IngestionStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, target_table: ddb.Table, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        five_minute_timer = events.Rule(
-            self,
-            "FiveMinuteTimer",
-            enabled=True,
-            schedule=events.Schedule.rate(core.Duration.minutes(5)))
-
-        self.five_minute_timer = five_minute_timer
-
         feed_scanner_lambda = pylambda.PythonFunction(self, "FeedScannerLambda",
                                                       function_name="CloudsatLHR Feed Scanner",
                                                       entry="lambdas/feed_scanner/",
@@ -26,4 +18,11 @@ class IngestionStack(core.Stack):
         target_table.grant_write_data(feed_scanner_lambda)
         target_table.grant_read_data(feed_scanner_lambda)
 
-        five_minute_timer.add_target(feed_scanner_lambda)
+        five_minute_timer = events.Rule(
+            self,
+            "FiveMinuteTimer",
+            enabled=True,
+            schedule=events.Schedule.rate(core.Duration.minutes(5)),
+            targets=[feed_scanner_lambda])
+
+        self.five_minute_timer = five_minute_timer
